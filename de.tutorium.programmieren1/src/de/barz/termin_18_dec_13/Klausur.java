952381735c693096777d05c37caf595c26202b5c
Sebastian Barz <Sebbi@SebbiDuo.localdomain>
2013-12-18 13:59:15 +0100

    Initial commit for 18.Dec.2013

---
+++ de.tutorium.programmieren1/src/de/barz/termin_18_dec_13/Klausur.java
@@ -0,0 +1,17 @@
+package de.barz.termin_18_dec_13;
+
+public class Klausur {
+	
+	private String name_ref;
+	
+	private int fachnummer;	
+	private int anzahlStudenten;
+	
+	private double notenSchnitt;
+	
+	public static final int DEFAULTWERT = -1;
+	public static final double DEFAULTNOTE = -1.0;
+
+	
+
+}
